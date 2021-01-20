010551e1778bf5d3738b65274c64de0b5f7c1b03
Federico Badini <federico@inkofpixel.com>
2021-01-20 15:37:47 +0100

    Header z-index set to 10

--- packages/stack/src/views/Header/HeaderContainer.tsx
+++ packages/stack/src/views/Header/HeaderContainer.tsx
@@ -142,9 +142,10 @@ export default function HeaderContainer({
                 style={
                   // Avoid positioning the focused header absolutely
                   // Otherwise accessibility tools don't seem to be able to find it
-                  (mode === 'float' && !isFocused) || headerTransparent
+                  ...(((mode === 'float' && !isFocused) || headerTransparent
                     ? styles.header
-                    : null
+										: null) || {}),
+										zIndex: 10
                 }
               >
                 {header !== undefined ? header(props) : <Header {...props} />}
